a41667e292db8fbe559a97d899d750c89ee5ec35
pamelanavarro <pamela.navarroha@udlap.mx>
2017-04-01 16:44:29 -0600

    My Django Girls app, first commit

---
+++ test1.py
@@ -0,0 +1,22 @@
+# if 1 > 2:print('el primero es mayor')
+# #elif 1>0:print('el numero es 1')
+# else:print('el primero no es mayor')
+#
+# def hi(name, saludo):
+#
+#     if name=='Azu':print('Hi there!'+saludo)
+# elif name=='Roberto',:print('How are you?'+saludo)
+# hi('Roberto', 'Paco')
+
+
+
+def hi(name):
+    print('Hi ' + name + '!')
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+
+for i in range(1, 6):
+    print(i)
